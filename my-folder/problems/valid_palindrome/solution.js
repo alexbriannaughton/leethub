4a7bd26bc485a1a821d83c7f52a3b9540b7d30d8
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-02-19 19:12:13 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

--- my-folder/problems/valid_palindrome/solution.js
+++ my-folder/problems/valid_palindrome/solution.js
@@ -2,14 +2,40 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    let string = s.replace(/[\W_]+/g,"").toLowerCase()
+var isPalindrome = function (s) {
     
-    let mid = Math.floor(string.length / 2)
+    // if (s.length === 1) return true
+
+    let i = 0
+    let j = s.length - 1
     
-    for (let i = 0; i < mid; i++) {
-        if (string[i] !== string[(string.length - 1) - i]) return false
+    function isValidChar(c) {
+        return (c.charCodeAt(0) > 96 && c.charCodeAt(0) < 123) || (c.charCodeAt(0) > 47 && c.charCodeAt(0) < 58)
+    }
+  
+    while (i < j) {
+        const curChar1 = s[i].toLowerCase()
+        const curChar2 = s[j].toLowerCase()
+        
+        if (!isValidChar(curChar1)) {
+            i++
+            continue
+        }
+
+        if (!isValidChar(curChar2)) {
+            j--
+            continue
+        }
+    
+        if (curChar1 !== curChar2) return false
+
+        i++
+        j--
     }
     
+    // if (!isValidChar(s[i]) && !isValidChar(s[j])) {
+    //     return false
+    // }
+
     return true
 };
