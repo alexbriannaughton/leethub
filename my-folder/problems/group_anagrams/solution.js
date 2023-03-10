1f8cb6300d28b2906ef9e43fb05722c1d16c1c93
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-03-10 01:00:16 +0000

    Sync LeetCode submission - Group Anagrams (javascript)

---
+++ my-folder/problems/group_anagrams/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let obj = {}
+    
+    for (str of strs) {
+        const letters = str.split("").sort().join("")
+        obj[letters] ? obj[letters].push(str) : obj[letters] = [str]
+    }
+    return Object.values(obj)
+};
