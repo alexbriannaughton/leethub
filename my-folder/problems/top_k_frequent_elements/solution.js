2f1be7606a6f745fbd5f6ca2defe4dfa40b01c9c
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-05-22 23:07:48 +0000

    Sync LeetCode submission - Top K Frequent Elements (javascript)

--- my-folder/problems/top_k_frequent_elements/solution.js
+++ my-folder/problems/top_k_frequent_elements/solution.js
@@ -3,18 +3,26 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    let obj = {}
-    for (num of nums) {
-        obj[num] ? obj[num]++ : obj[num] = 1
-    }
-    let ordered = Object.values(obj).sort(function(a, b){return a - b})
-    let topK = ordered.slice(-k)
-    let answer = []
-    for (num of Object.keys(obj)) {
+var topKFrequent = function (nums, k) {
+    const obj = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        obj[nums[i]]
+            ? obj[nums[i]]++
+            : obj[nums[i]] = 1;
+    };
+
+    const topK = Object.values(obj)
+        .sort((a, b) => a - b)
+        .slice(-k);
+
+    const answer = [];
+
+    for (num in obj) {
         if (topK.includes(obj[num])) {
-            answer.push(parseInt(num))
-        }
-    }
-    return answer
+            answer.push(parseInt(num));
+        };
+    };
+
+    return answer;
 };
