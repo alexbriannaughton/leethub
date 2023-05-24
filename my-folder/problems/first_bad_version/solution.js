41b51e601cfff1642f1968927450a705438f0670
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-05-24 22:51:24 +0000

    Sync LeetCode submission - First Bad Version (javascript)

---
+++ my-folder/problems/first_bad_version/solution.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let a = 1;
+        while(!isBadVersion(a)) {
+            a++;
+        }
+        return a
+    };
+};
