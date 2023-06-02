076c0eaa05ef978fee78f7924c8d3381ccfa13c5
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-06-02 21:04:08 +0000

    Sync LeetCode submission - Form Smallest Number From Two Digit Arrays (javascript)

---
+++ my-folder/problems/form_smallest_number_from_two_digit_arrays/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var minNumber = function(nums1, nums2) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) return nums1[i];
+    }
+
+    const n1 = nums1[0];
+    const n2 = nums2[0];
+
+    return [n1, n2].sort().join("");
+};
