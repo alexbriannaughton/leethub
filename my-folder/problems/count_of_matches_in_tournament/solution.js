2e6e9ce1dddc4dddcdf7943599e91da988d194f7
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-06-06 01:45:01 +0000

    Sync LeetCode submission - Count of Matches in Tournament (javascript)

--- my-folder/problems/count_of_matches_in_tournament/solution.js
+++ my-folder/problems/count_of_matches_in_tournament/solution.js
@@ -6,29 +6,17 @@ var numberOfMatches = function(n) {
     let count = 0;
     while (n > 1) {
         if (n === 2) {
-            count++
-            break
+            count++;
+            break;
         }
         if (n % 2 === 0) {
-            count += n / 2
-            n = n / 2
+            count += n / 2;
+            n = n / 2;
         }
         else {
-            count += (n - 1) / 2
-            n = (n - 1) / 2 + 1
+            count += (n - 1) / 2;
+            n = (n - 1) / 2 + 1;
         }
     }
     return count
-    
-    // function go(n) {
-    //     if (n === 1) return count
-    //     if (n === 2) return ++count
-    //     if (n % 2 === 0) {
-    //         count += n / 2
-    //         return go(n / 2)
-    //     }
-    //     count += (n - 1) / 2
-    //     return go((n - 1) / 2 + 1)
-    // }
-    // return go(n)
 };
