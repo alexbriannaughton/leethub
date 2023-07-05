9df8811ff0040d13baec52001f97dfebc3c96a04
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-07-05 23:45:58 +0000

    Sync LeetCode submission - Roman to Integer (javascript)

---
+++ my-folder/problems/roman_to_integer/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const obj = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let sum = 0;
+    for (let i = 0; i < s.length; i++) {
+        const cur = obj[s[i]];
+        const next = obj[s[i+1]];
+        if (cur >= next || !next) sum += cur;
+        else sum -= cur;
+    }
+    return sum;
+};
