1a68c11ad68f09b5010682d02f9e6861b4a7f328
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-07-07 22:56:51 +0000

    Sync LeetCode submission - Student Attendance Record I (javascript)

--- my-folder/problems/student_attendance_record_i/solution.js
+++ my-folder/problems/student_attendance_record_i/solution.js
@@ -3,19 +3,12 @@
  * @return {boolean}
  */
 var checkRecord = function(s) {
-    let a = 0
-
+    let aCount = 0;
+    let late = false;
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === "A") {
-            a++
-        }
-
-        if (a > 1) return false
-
-        if (s[i] === "L" && s[i+1] === "L" && s[i+2] === "L") {
-            return false
-        }
+        if (s[i] === 'A') aCount++;
+        if (aCount >= 2) return false;
+        if (s[i] === 'L' && s[i+1] === 'L' && s[i+2] === 'L') return false;
     }
-
-    return true
+    return true;
 };
