7909ea9ea6e0f6b7f55f2513f4e3a5134df5deac
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-07-20 01:19:51 +0000

    Sync LeetCode submission - Flatten Deeply Nested Array (javascript)

--- my-folder/problems/flatten_deeply_nested_array/solution.js
+++ my-folder/problems/flatten_deeply_nested_array/solution.js
@@ -4,13 +4,11 @@
  * @return {any[]}
  */
 function flat(arr, n, ans = [], level = 0) {
-  for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === 'number' || level === n) {
-      ans.push(arr[i]);
-    } else {
-      flat(arr[i], n, ans, level + 1);
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' || level === n) {
+            ans.push(arr[i]);
+        }
+        else flat(arr[i], n, ans, level + 1);
     }
-  }
-
-  return ans;
+    return ans;
 }
