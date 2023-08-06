eef854cfec5132c6b9085e1e35eac9b1de259583
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-08-06 01:02:08 +0000

    Sync LeetCode submission - Rearrange Array Elements by Sign (javascript)

--- my-folder/problems/rearrange_array_elements_by_sign/solution.js
+++ my-folder/problems/rearrange_array_elements_by_sign/solution.js
@@ -2,14 +2,18 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var rearrangeArray = function(nums) {
+var rearrangeArray = function (nums) {
     const res = [];
-    const index = {pos: 0, neg: 1};
+    const signIndex = {
+        pos: 0,
+        neg: 1
+    };
 
     for (num of nums) {
         const sign = num >= 0 ? 'pos' : 'neg';
-        res[index[sign]] = num;
-        index[sign] += 2;
+        const index = signIndex[sign];
+        res[index] = num;
+        signIndex[sign] += 2;
     }
 
     return res;
