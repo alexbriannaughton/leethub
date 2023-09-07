6ff2cafa973ffe3ebca2f146ff03e811d3568489
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-09-07 01:04:47 +0000

    Sync LeetCode submission - Sort Colors (javascript)

--- my-folder/problems/sort_colors/solution.js
+++ my-folder/problems/sort_colors/solution.js
@@ -2,13 +2,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
+var sortColors = function (nums) {
     const counts = {
         0: 0,
         1: 0,
-        2: 0
     }
-
+    
     for (n of nums) {
         counts[n]++
     }
@@ -22,9 +21,6 @@ var sortColors = function(nums) {
             nums[i] = 1
             counts[1]--
         }
-        else {
-            nums[i] = 2
-        }
+        else nums[i] = 2
     }
-    
 };
