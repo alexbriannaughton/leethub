1fdba810d82b41a32da4609c33881fbee292482b
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-09-11 22:49:39 +0000

    Sync LeetCode submission - Group the People Given the Group Size They Belong To (javascript)

--- my-folder/problems/group_the_people_given_the_group_size_they_belong_to/solution.js
+++ my-folder/problems/group_the_people_given_the_group_size_they_belong_to/solution.js
@@ -3,17 +3,18 @@
  * @return {number[][]}
  */
 var groupThePeople = function(groupSizes) {
-    let obj = {}
-    let answer = []
+    const output = []
+    const groups = {}
     for (let i = 0; i < groupSizes.length; i++) {
-        if (!obj[groupSizes[i]]) {
-            obj[groupSizes[i]] = []
-        }
-        obj[groupSizes[i]].push(i)
-        if (obj[groupSizes[i]].length === groupSizes[i]) {
-            answer.push(obj[groupSizes[i]])
-            obj[groupSizes[i]] = []
+        const curIDsGroupSize = groupSizes[i]
+        groups[curIDsGroupSize]
+            ? groups[curIDsGroupSize].push(i)
+            : groups[curIDsGroupSize] = [i]
+
+        if (groups[curIDsGroupSize].length === curIDsGroupSize) {
+            output.push(groups[curIDsGroupSize])
+            groups[curIDsGroupSize] = []
         }
     }
-    return answer
+    return output
 };
