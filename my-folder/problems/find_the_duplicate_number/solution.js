c2d420bc791bdd590a140c418a3a18aa299644ad
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-09-19 17:27:35 +0000

    Sync LeetCode submission - Find the Duplicate Number (javascript)

--- my-folder/problems/find_the_duplicate_number/solution.js
+++ my-folder/problems/find_the_duplicate_number/solution.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var findDuplicate = function (nums) {
-    let slow = nums[0];
-    let fast = nums[nums[0]];
+    let slow = nums[0], fast = slow
 
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
+    do {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+    } while (slow !== fast)
+
+    slow = nums[0]
+
+    while (slow !== fast) {
+        slow = nums[slow]
+        fast = nums[fast]
     }
-    
-    slow = 0;
 
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[fast];
-    }
-
-    return slow;
+    return slow
 };
