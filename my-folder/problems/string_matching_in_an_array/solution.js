91a5034270a0b0ea231b080f338a5c4f6f11c38a
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2023-10-14 20:02:19 +0000

    Sync LeetCode submission - String Matching in an Array (javascript)

--- my-folder/problems/string_matching_in_an_array/solution.js
+++ my-folder/problems/string_matching_in_an_array/solution.js
@@ -2,11 +2,11 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var stringMatching = function(words) {
-    return words.filter((word) => {
-        for (const w of words) {
-            if (w === word) continue
-            if (w.includes(word)) return word
-        }
+var stringMatching = function (words) {
+    return words.filter((curWord) => {
+        return words.find((word) => {
+            return word.includes(curWord) && word != curWord
+                ? word : null
+        })
     })
 };
