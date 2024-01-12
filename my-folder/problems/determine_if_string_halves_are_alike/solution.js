8ad7081b600d529205139854294ba8baed4ad466
Alex Naughton <109716310+alexbriannaughton@users.noreply.github.com>
2024-01-12 18:54:16 +0000

    Sync LeetCode submission - Determine if String Halves Are Alike (javascript)

--- my-folder/problems/determine_if_string_halves_are_alike/solution.js
+++ my-folder/problems/determine_if_string_halves_are_alike/solution.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-    const a = s.split('').splice(s.length/2);
-    const b = s;
-    const vowels = 'a,e,i,o,u,A,E,I,O,U';
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
     let aCount = 0;
     let bCount = 0;
     for (let i = 0; i < s.length/2; i++) {
-        if (vowels.includes(a[i])) aCount++;
-        if (vowels.includes(b[i])) bCount++;
+        if (vowels.has(s[i])) aCount++;
+        if (vowels.has(s[i + s.length/2])) bCount++;
     }
     return aCount === bCount;
 };
